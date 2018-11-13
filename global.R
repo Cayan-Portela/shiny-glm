ab625c3ccd73897e5776e74a70c2014f9b143612
Unknown <cayanportela@hotmail.com>
2018-11-13 18:52:22 -0200

    check installed packages

--- global.R
+++ global.R
@@ -1,3 +1,9 @@
+list.of.packages <- c("DT", "car","hnp","purrr","dplyr","shiny","scales",
+                      "plotly","ggplot2","ggthemes","ggfortify","shinydashboard",
+                      "shinycssloaders")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(DT)
 library(car)
 library(hnp)
